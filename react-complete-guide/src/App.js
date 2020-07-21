e6ce75aaca9ede9c0cdfe2afafab5d92f8d45a34
unknown <emillyn_muniz@hotmail.com>
2020-07-21 00:56:42 -0300

    Curso React

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
+import styled from 'styled-components';
 import Radium, {StyleRoot} from  'radium';
 import person from './Person/Person';
+
+
+
+const StyledButton = styled.button`
+    background-color: green;
+    color: white;
+    font: inherit;
+    border: 1px solid blue;
+    padding: 8px;
+    cursor: pointer;
+
+    &:hover: {
+      background-color: lightgreen,
+      color: black
+    }
+`
 
 class App extends Component {
   state = {
@@ -102,16 +119,14 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={styles} 
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </StyledButton>
           {persons}
       </div>
-      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
@@ -131,25 +146,13 @@ class App2 extends Component{
   render(){
     return(
       <div className="App">
-      {/* <ol>
-        <li>Create TWO new components: UserInput and UserOutput</li>
-        <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-        <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-        <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-        <li>Add state to the App component ( the username) and pass the username to the UserOutput component</li>
-        <li>Add a method to manipulate the state ( an event-handler method)</li>
-        <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-        <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-        <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-        <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-      </ol> */}
-      <UserInput changed={this.usernameChangedHandler} currentName={this.state.username}/>
-      <UserOutput userName={this.state.username}/>
-      <UserOutput userName={this.state.username}/>
-      <UserOutput userName="Emillyn"/>
+        <UserInput changed={this.usernameChangedHandler} currentName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName="Emillyn"/>
     </div>
     )
   }
 }
 
-export default Radium(App);
+export default App;
