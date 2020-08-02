60e7fe125d93b0d62864f2e725e43ed80b6a2a95
unknown <emillyn_muniz@hotmail.com>
2020-08-02 12:19:00 -0300

    Higher Order Components (HOC)

--- react-complete-guide/styling--03-finished/src/components/Persons/Person/Person.js
+++ react-complete-guide/styling--03-finished/src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
 import React , {Component} from 'react';
 
-import classes from './Person.css';
+// import classes from './Person.css';
+import Aux from '../../../HocClass/Auxliar';
 
 class Person extends Component{
   render(){
-    return([
-        <p key="i1" onClick={this.props.click}>
+    return(
+      <Aux>
+        <p  onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
-        </p>,
-        <p key="i2">{this.props.children}</p>,
-        <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
-    ])
+        </p>
+        <p>{this.props.children}</p>
+        <input  type="text" onChange={this.props.changed} value={this.props.name} />
+      </Aux>
+    )
   }
 };
 
--- react-complete-guide/styling--03-finished/src/containers/App.js
+++ react-complete-guide/styling--03-finished/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../HocClass/WithClass';
 
 class App extends Component {
   state = {
@@ -57,7 +58,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
         {this.state.showCockpit ? <Cockpit
           title={this.props.appTitle}
@@ -66,7 +67,7 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         />: null } 
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
